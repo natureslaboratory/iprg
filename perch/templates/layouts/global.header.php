94f60649593e2d322aef9139e3bf65920f76caa4
Jack Barber <jack@natureslaboratory.co.uk>
2021-03-08 16:23:00 +0000

    Change font

--- perch/templates/layouts/global.header.php
+++ perch/templates/layouts/global.header.php
@@ -20,10 +20,10 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin />
     <link rel="preconnect" href="https://kit.fontawesome.com" crossorigin />
     
-	<link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,700;1,400;1,700&display=swap" />
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,700;1,400;1,700&display=swap" media="print" onload="this.media='all'" />
+	<link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Ropa+Sans&display=swap" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Ropa+Sans&display=swap" media="print" onload="this.media='all'" />
 	<noscript>
-	  <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+	  <link href="https://fonts.googleapis.com/css2?family=Ropa+Sans&display=swap" rel="stylesheet"> 
 	</noscript> 
     <link rel="stylesheet" href="/assets/css/stylesheet.css?v=<?php echo rand(); ?>">
 </head>
