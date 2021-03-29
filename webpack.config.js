4df8fa1658e17e68228bb18cdc420be9711c6e70
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-03-29 09:27:18 +0100

    added ?v to bundle

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: "bundle1.js",
+        filename: "bundle.js",
         path: path.resolve(__dirname, "assets", "js")
     },
     watch: true,
