b5c4441e7062a488992f9bf80ed2f0f2e33bca7d
Jack Barber <jack@natureslaboratory.co.uk>
2021-10-26 16:36:25 +0100

    add hivechat

---
+++ perch/templates/pages/hivechat.php
@@ -0,0 +1,8 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); ?>
+<?php perch_layout('global.header'); ?>
+
+<?php 
+	perch_content('Page Content'); 
+?>
+
+<?php perch_layout('global.footer'); ?>
